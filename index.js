54814611e205dd2149d3874fa42a055dd07786e7
KieuTrungHieu <kieutrunghieu104@gmail.com>
2026-09-09 10:00:19 +0700

    init project

---
+++ index.js
@@ -0,0 +1,14 @@
+// trunghieu
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+})
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+})
